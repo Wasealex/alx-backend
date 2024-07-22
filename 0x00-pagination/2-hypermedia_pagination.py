08918bc0f097b40183f6742406b56602bf2c4aee
Wasealex <wasealex27@gmail.com>
2024-07-22 10:00:15 +0300

    Add pagination functionality to Server class

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -49,3 +49,25 @@ class Server:
         start_index = indexes[0]
         end_index = indexes[1]
         return self.dataset()[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Finds the correct indexes to paginate the dataset correctly
+        and return the appropriate page of the dataset
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        indexes = index_range(page, page_size)
+        start_index = indexes[0]
+        end_index = indexes[1]
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        data = self.dataset()[start_index:end_index]
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }

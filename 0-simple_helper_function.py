b780278b7bf8a0224c2ff64c3ca7e5d70a86dd10
Wasealex <wasealex27@gmail.com>
2024-07-22 09:15:01 +0300

    function index_range created that return tuple of indexs

---
+++ 0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is a simple helper function that returns 
+a tuple of size two containing:
+a start index and an end index
+"""
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+    Return a tuple of size two containing a start index 
+    and an end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)

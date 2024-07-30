929dcfcce5733671a6a8a351bd88b2db5b7c3331
Wasealex <wasealex27@gmail.com>
2024-07-30 14:42:51 +0300

    chore: Refactor Flask app for internationalization support

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""a simple flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config class
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """get locale
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """GET method
+    """
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

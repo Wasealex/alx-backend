5231d8646249e75a33ddb29fac0a0c3e538a32a5
Wasealex <wasealex27@gmail.com>
2024-07-30 15:24:13 +0300

    chore: Update render_template path to '4-index.html'

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -24,6 +24,7 @@ def get_locale():
     """
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
+        print (locale)
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
